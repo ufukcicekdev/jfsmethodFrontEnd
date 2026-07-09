92b3c68f97c44564ebd8e26f1d7fdce55711e183
EAGLEEARTH <ufukcicek199@gmail.com>
2026-07-09 22:05:56 +0300

    push

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "jfsmethod.com" }],
+        destination: "https://www.jfsmethod.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
